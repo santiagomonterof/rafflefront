c05f809f4e014da04ba5654c2c493b2d85a3f7f6
santiagomonterof <amountconfused@gmail.com>
2024-05-21 09:34:53 -0400

    chore: Update NextUI dependencies and remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,164 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
+"use client"
+
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { GithubIcon, IconGoogle } from "@/components/icons";
+import { Card } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { useState } from "react";
+
+
+
 
 export default function Home() {
+	const [option, setOption] = useState(0);
+	const Login = () => {
+		return (
+			<Card className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
+				<h1 className="text-2xl font-bold">Log In</h1>
+				<form className="space-y-6">
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="email"
+							label="Email Address"
+							type="email"
+						/>
+					</div>
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="password"
+							label="Password"
+							type="password"
+						/>
+					</div>
+					<div className="flex items-center justify-between">
+						<div className="flex items-center">
+							<input
+								className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+								id="remember-me"
+								type="checkbox"
+							/>
+							<label className="ml-2 block text-sm" htmlFor="remember-me">
+								Remember me
+							</label>
+						</div>
+						<a className="text-sm text-blue-500 hover:underline" href="#">
+							Forgot password?
+						</a>
+					</div>
+					<Button className="w-full dark:bg-blue-600">Log In</Button>
+				</form>
+				<div className="my-6 flex items-center justify-center">
+					<div className="h-px w-full bg-gray-600" />
+					<span className="px-3 text-sm text-gray-400">OR</span>
+					<div className="h-px w-full bg-gray-600" />
+				</div>
+				<div className="flex flex-col space-y-4">
+					<Button
+						color="primary"
+						className="flex items-center justify-center space-x-2">
+						<IconGoogle className="h-5 w-5 text-red-500" />
+						<span>Continue with Google</span>
+					</Button>
+					<Button className="flex items-center justify-center space-x-2 bg-[#333] text-white hover:bg-[#444]">
+						<GithubIcon className="h-5 w-5" />
+						<span>Continue with Github</span>
+					</Button>
+				</div>
+				<div className="mt-4 text-center">
+					<span className="text-sm text-gray-400">Need to create an account?</span>
+					<a className="ml-1 text-sm text-blue-500 hover:underline" href="#"
+						onClick={() => setOption(1)}>
+						Sign Up
+					</a>
+				</div>
+
+			</Card>
+		);
+	}
+
+	const Register = () => {
+		return (
+			<Card className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
+				<h1 className="text-2xl font-bold">Sign Up</h1>
+				<form className="space-y-6">
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="name"
+							label="Name"
+							type="text"
+						/>
+					</div>
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="lastname"
+							label="Last Name"
+							type="text"
+						/>
+					</div>
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="phone"
+							label="Phone Number"
+							type="number"
+						/>
+					</div>
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="email"
+							label="Email Address"
+							type="email"
+						/>
+					</div>
+					<div className="space-y-2">
+						<Input
+							className="w-full"
+							id="confirm-password"
+							label="Confirm Password"
+							type="password"
+						/>
+					</div>
+					<Button className="w-full dark:bg-blue-600">Sign Up</Button>
+				</form>
+				<div className="my-6 flex items-center justify-center">
+					<div className="h-px w-full bg-gray-600" />
+					<span className="px-3 text-sm text-gray-400">OR</span>
+					<div className="h-px w-full bg-gray-600" />
+				</div>
+				<div className="mt-4 text-center">
+					<span className="text-sm text-gray-400">Already have an account?</span>
+					<a className="ml-1 text-sm text-blue-500 hover:underline" href="#"
+						onClick={() => setOption(0)}>
+						Log In
+					</a>
+				</div>
+			</Card>
+		);
+	}
+
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+		<section className="flex items-center justify-around gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<h1 className={title()}>Join&nbsp;</h1>
+				<h1 className={title()}>our&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>exclusive&nbsp;</h1>
 				<br />
 				<h1 className={title()}>
-					websites regardless of your design experience.
+					giveaways and win fantastic prizes
 				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
+				<h2 className={subtitle({ class: "mt-6" })}>
+					Don't miss out on the fun – enter now!
 				</h2>
 			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
+			{
+				option === 0 ? <Login /> : <Register />
+			}
 		</section>
 	);
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,10 +57,10 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
+						<p className="font-bold text-inherit">RAFFLE</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+				{/* <ul className="hidden lg:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
@@ -75,7 +75,7 @@ export const Navbar = () => {
 							</NextLink>
 						</NavbarItem>
 					))}
-				</ul>
+				</ul> */}
 			</NavbarContent>
 
 			<NavbarContent
@@ -83,21 +83,15 @@ export const Navbar = () => {
 				justify="end"
 			>
 				<NavbarItem className="hidden sm:flex gap-2">
-					<Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-						<TwitterIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-						<DiscordIcon className="text-default-500" />
-					</Link>
 					<Link isExternal href={siteConfig.links.github} aria-label="Github">
 						<GithubIcon className="text-default-500" />
 					</Link>
 					<ThemeSwitch />
 				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-				<NavbarItem className="hidden md:flex">
+				{/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
+				{/* <NavbarItem className="hidden md:flex">
 					<Button
-            isExternal
+						isExternal
 						as={Link}
 						className="text-sm font-normal text-default-600 bg-default-100"
 						href={siteConfig.links.sponsor}
@@ -106,7 +100,7 @@ export const Navbar = () => {
 					>
 						Sponsor
 					</Button>
-				</NavbarItem>
+				</NavbarItem> */}
 			</NavbarContent>
 
 			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -117,7 +111,7 @@ export const Navbar = () => {
 				<NavbarMenuToggle />
 			</NavbarContent>
 
-			<NavbarMenu>
+			{/* <NavbarMenu>
 				{searchInput}
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{siteConfig.navMenuItems.map((item, index) => (
@@ -127,8 +121,8 @@ export const Navbar = () => {
 									index === 2
 										? "primary"
 										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
+											? "danger"
+											: "foreground"
 								}
 								href="#"
 								size="lg"
@@ -138,7 +132,7 @@ export const Navbar = () => {
 						</NavbarMenuItem>
 					))}
 				</div>
-			</NavbarMenu>
+			</NavbarMenu> */}
 		</NextUINavbar>
 	);
 };
